f33ae6e18898cb20c5359490807b8a4b2a36e5fe
ilance <ilance@uoregon.edu>
2018-03-12 15:59:48 -0700

    Started driver folder. Need to learn how to multithread this. Driver should be no more than 10 lines.

---
+++ driver/driver.cpp
@@ -0,0 +1,8 @@
+int main(){
+	mutex m;
+	buffer [];
+	TwitchIRC bot = TwitchIRC("NICK crowdsourcechess\r\n", "PASS oauth:knx6ly88bw0bqwmvckrbp2r288ffc6\r\n", queue);
+	bot.start();
+	//Read from buffer
+	justinsbot.start(queue)
+}
