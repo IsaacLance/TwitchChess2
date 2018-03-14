88fc11f94f0c2199634d51c21456b918404798bc
ilance <ilance@uoregon.edu>
2018-03-13 21:30:23 -0700

    About to drastically change the way messages are received, committing so I can come back to this version if I fail.

--- reader/TwitchIRC.cpp
+++ reader/TwitchIRC.cpp
@@ -25,134 +25,133 @@ http://man7.org/linux/man-pages/man3/getaddrinfo.3.html
 #include <chrono>
 #include <thread>
 
-
-
 using namespace std; //FIXME: using std:"function" may be better
 #define PORT "6667" //Required by TWITCH.TV
 #define MAXDATASIZE 100
 #define URL "irc.chat.twitch.tv"
+#define DEFAULT_CHANNEL "#doyouphilsme"
 //#define URL "irc.us.ircnet.net"
 //FIXME: Edit max size? It could actually be way smaller but its risky?
 //Fixme: Change error handling
-TwitchIRC::TwitchIRC(std::string username,std::string oauth){//,char* outbuff){char* name, 
-	//nick = name;
+TwitchIRC::TwitchIRC(string username,string oauth, queue<string> output){
 	user = username;
 	pass = oauth;
-	//buff = outbuff;
+	output = output;
 }
+
 TwitchIRC::~TwitchIRC(){//FIXME: This might need to be virtual? What happens if we close no s?
 	close(my_socket);
 }
 //Public Methods
 void TwitchIRC::start(){
 	cout << "BOT START\n" << flush;
-	struct addrinfo hints;// "hints" is an optional pointer to the struct addrinfo-
-						  //-this confused me for a long time! haha
-	struct addrinfo *servinfo;// points to the results
+	Setup();
+	Login();
+	Join(DEFAULT_CHANNEL);
 	
-	//set some values
-	setup = true;
-	port = PORT;
-	url = URL;
-	
-	
-	int status;
-	//setup hints
-	memset(&hints, 0 ,sizeof(hints)); //set the struct to being empty
-	hints.ai_family = AF_UNSPEC; //unspecified (IPv4 or v6 accepted)
-	hints.ai_socktype = SOCK_STREAM; //other option: sock_dgram, not for us though
-	//hints.ai_flags = AI_PASSIVE; //I'm 90% sure this is unneeded
-	//Setup structs/sockets/ then connect, all with error checking
-
-	if ((status = getaddrinfo(url.c_str(), port.c_str(), &hints, &servinfo)) !=0){
-		setup = false;
-		printf("ERROR 1");
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status)); //gai_strerror: return text string describing error value from getaddrinfo()
-	}
-
-	if ((my_socket = socket(servinfo->ai_family, servinfo->ai_socktype,servinfo->ai_protocol)) == -1){
-		printf("ERROR 2");
-		perror("client: socket");
-	}
-
-	if (connect(my_socket, servinfo->ai_addr, servinfo->ai_addrlen) == -1){
-		printf("ERROR3");
-		perror("Client connect");
-		close(my_socket);
-	}
-
-	//freeaddrinfo is a function that takes a pointer to an addrinfo instance and frees associated memory
-	freeaddrinfo(servinfo);
-	//so basically free(servinfo) but, like, the way that works
+	//RUN main loop
 	int bytes_recieved;
 	char buff[MAXDATASIZE];
-	
-	int count = 0; //fixme: why is this here
-	//TEMP: connect to a channel and print its chat in console
-	
-	//login
-	cout << "send data:\n" << flush;
-	sendData(pass);
-	sendData(user);
-	cout << "begin loop\n" << flush;
 	while (true){//fixme: main loop should be in a different method
-		count++;
 		bytes_recieved = recv(my_socket, buff, MAXDATASIZE -1, 0);
-		mutex.lock(buffer);
-		buff[bytes_recieved] = '\0';
-		mutex.unlock(buffer);
-		cout <<buff<< "\n" << flush;
-		this_thread::sleep_for(1s);
+		buff[bytes_recieved] = '\0';//Null terminate so we can parse
+		//cout <<buff;//<< flush;
+		Parse(buff);
+		this_thread::sleep_for(10ms);
 		//this_thread::sleep_for(chrono::milliseconds(300));
 		//cout << buff;
 		
 	}	
 }
 	
-	bool TwitchIRC::charPresent( string block, string target){
-		bool found = false; //Fixing the absolutely STELLAR "example" code, now this function returns true or false instead of true or 0, yeah I know.
-		//Loop through block,
-		for (int i = 0; i < block.length(); i++){
-			if (block[i] == target[0]){ //if we match the first letter,
-				found = true;
-				//keep trying to match until we get a full match.
-				for (int n = 1; n < target.length(); n++){
-					if (block[i+n] =! target[n]){ // Starting check at the 1 index character, we checked 0
-						found = false;
-					}
-				}
-			}
+	size_t TwitchIRC::substringPresent(string block, string target){
+		size_t found = block.find(target);
+		if (found!=string::npos){
+			return true;
 		}
-		return found;
+		return false;
 	}
 
 	
 //Private Methods
-	bool TwitchIRC::isConnected(char* buf){
+	/*bool TwitchIRC::isConnected(char* buf){ //DELETEME: This is useless now?
 		//returns true if '/MOTD' is found in the input string. This is a very trivial function but I'm leaving it in for now.
 		//If we find /MOTD then its ok to join a channel #FIXME: LEARN WHY THIS IS
 		return (charPresent(buf, "/MOTD"));
+	}*/
+	bool TwitchIRC::Setup(){
+		struct addrinfo hints;// "hints" is an optional pointer to the struct addrinfo-
+						  //-this confused me for a long time! haha
+		struct addrinfo *servinfo;// points to the results
+	
+		//set some values
+		setup = true;
+		port = PORT;
+		url = URL;
+		int status;
+		//setup hints
+		memset(&hints, 0 ,sizeof(hints)); //set the struct to being empty
+		hints.ai_family = AF_UNSPEC; //unspecified (IPv4 or v6 accepted)
+		hints.ai_socktype = SOCK_STREAM; //other option: sock_dgram, not for us though
+		//Setup structs/sockets/ then connect, all with error checking
+		if ((status = getaddrinfo(url.c_str(), port.c_str(), &hints, &servinfo)) !=0){
+			setup = false;
+			printf("ERROR 1");
+			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status)); //gai_strerror: return text string describing error value from getaddrinfo()
+		}
+
+		if ((my_socket = socket(servinfo->ai_family, servinfo->ai_socktype,servinfo->ai_protocol)) == -1){
+			printf("ERROR 2");
+			perror("client: socket");
+		}
+
+		if (connect(my_socket, servinfo->ai_addr, servinfo->ai_addrlen) == -1){
+			printf("ERROR3");
+			perror("Client connect");
+			close(my_socket);
+		}
+
+		//freeaddrinfo is a function that takes a pointer to an addrinfo instance and frees associated memory
+		freeaddrinfo(servinfo);
 	}
 	
-	bool TwitchIRC::sendData(std::string msg){ //the bool return makes this method a sort of "try" method.
+	bool TwitchIRC::sendData(string msg){ //the bool return makes this method a sort of "try" method.
 		int bytes_sent = send(my_socket, msg.c_str(), msg.length(),0);
 		cout << "bytes sent:" <<bytes_sent << "\n msg: " << msg << "\n";
-		if (bytes_sent == 0){return false;}
-		
+		if (bytes_sent == 0){
+			return false;}
 		return true;
 	}
 
-	void TwitchIRC::sleep(double seconds){
-		clock_t start;
-		double duration = 0;
-		start = clock();
-		while (duration < 3){
-			duration = (clock() - start) / (double) CLOCKS_PER_SEC;
-		}
-		cout << "Awake" << flush;
+	bool TwitchIRC::Join(string channel){
+		string msg = "JOIN " + channel + "\r\n";
+		return sendData(msg);
 	}
 	
+	bool TwitchIRC::Login(){
+		string P = "PASS oauth:" + pass + "\r\n";
+		string N = "NICK " + user + "\r\n";
+		return(sendData(P) && sendData(N));
+	}
 	
+	bool TwitchIRC::Parse(char* line){
+		string msg(line);
+		cout <<"Parsed:"<< msg;//<< flush;
+		/*
+		if (msg.find("PING") == 0){
+			sendData("PONG :tmi.twitch.tv");
+			return false; //Don't do anything more
+		}
+		if (msg[0] != ':'){
+			cout << "invalid msg recieved: didn't start with ':'\n" << flush;
+			return false;
+		}
+		size_t name_x = 1;
+		size_t name_y = msg.find("!");
+		string name = msg.substr(name_x, name_y - name_x);
+		cout<< "recieved message from:  " << name << "\n" << flush;
+		return true;*/
+	}
 	/*void TwitchIRC::sendPong(char* buf){
 		
 	}*/
@@ -184,7 +183,8 @@ void TwitchIRC::start(){
 */
 int main(){
 	cout << "PROGRAM START\n"<< flush;
-	TwitchIRC bot = TwitchIRC("NICK crowdsourcechess\r\n", "PASS oauth:knx6ly88bw0bqwmvckrbp2r288ffc6\r\n");
+	queue<string> test;
+	TwitchIRC bot = TwitchIRC("crowdsourcechess", "knx6ly88bw0bqwmvckrbp2r288ffc6", test);
 	bot.start();
 	
 	return 0;
--- reader/TwitchIRC.hpp
+++ reader/TwitchIRC.hpp
@@ -4,17 +4,17 @@ Isaac Lance
 
 #ifndef TwitchIRC_HPP_
 #define TwitchIRC_HPP_
-
+#include <queue>
 #include <string>
 
 class TwitchIRC{
 	public:
 		//*structors
-		TwitchIRC(std::string username,std::string password);//char* outbuffchar* name, 
+		TwitchIRC(std::string username,std::string password, std::queue<std::string> output);
 		~TwitchIRC();
 		//Public Methods
 		void start();
-		bool charPresent(std::string block,std::string target);
+		size_t substringPresent(std::string block,std::string target);
 		//Public vars
 		bool setup;
 
@@ -22,16 +22,21 @@ class TwitchIRC{
 		//Private vars
 		std::string port;
 		std::string url;
-		int my_socket; //socket descriptor
-		std::string nick;
+		
 		std::string user;
 		std::string pass;
-		char* buff[100];
+		
+		int my_socket; //socket descriptor
+		std::queue<std::string> shared_queue;
+		//FIXME: DO I NEED THIS? char* buff[100];
 		//Private methods
-		bool isConnected(char* buf);
-		//char* timeNow();
-		void sleep(double seconds);
+		//bool isConnected(char* buf);
+		bool Setup();
 		bool sendData(std::string msg);
+		bool Join(std::string channel);
+		bool Login();
+		bool Parse(char* msg);
+		
 		//void sendPong(char* buf);
 		//void msgHandle(char* buf);
 };
